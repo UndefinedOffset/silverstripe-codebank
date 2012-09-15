d9b46bdcf001fb72f8023f6e681897905f8289ef
Ed Chipman <edchipman@gmail.com>
2012-09-15 01:48:52 +0000

    Added security extension so the version number is present in the header

---
+++ code/extensions/CodeBankSecurity.php
@@ -0,0 +1,15 @@
+<?php
+class CodeBankSecurity extends Extension {
+    /**
+     * Gets the current version of Code Bank
+     * @return {string} Version Number Plus Build Date
+     */
+    public function getCodeBankVersion() {
+        if(CB_VERSION=='@@VERSION@@') {
+            return _t('CodeBank.DEVELOPMENT_BUILD', '_Development Build');
+        }
+        
+        return CB_VERSION.' '.CB_BUILD_DATE;
+    }
+}
+?>
